dafe62ccba8a6ff830896d9c7729dbd0cfea40b5
Itamar Junior <cdajuniorf@gmail.com>
2025-02-10 20:31:04 -0300

    feat: Diretivas v-else-if e v-else.

--- src/componentes/Exemplo02Condicional.vue
+++ src/componentes/Exemplo02Condicional.vue
@@ -1,11 +1,15 @@
 <!-- SCRIPT -->
 <script setup>
-// Variável
+// Variáveis 
 let exibir = false;
+let media = 8;
 </script>
-
 
 <!-- HTML -->
 <template>
   <h1 v-if="exibir">Hello World!</h1>
+
+  <p v-if="media >= 7">Aprovado(a)</p>
+  <p v-else-if="media >= 5">Em exame</p>
+  <p v-else>Reprovado(a)</p>
 </template>
