774530fbe5b35d9299c298d241900e4db6a75978
Itamar Junior <cdajuniorf@gmail.com>
2025-02-10 20:23:11 -0300

    feat: Diretiva v-if.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <!-- SCRIPT-->
 <script setup>
 // Importação dos componentes
-import Exemplo01 from './componentes/Exemplo01Interpolacao.vue';
+// import Exemplo01 from './componentes/Exemplo01Interpolacao.vue';
+import Exemplo02 from './componentes/Exemplo02Condicional.vue';
 </script>
 
 
 <!-- HTML -->
 <template>
-  <Exemplo01 />
+  <Exemplo02 />
 </template>
---
+++ src/componentes/Exemplo02Condicional.vue
@@ -0,0 +1,11 @@
+<!-- SCRIPT -->
+<script setup>
+// Variável
+let exibir = false;
+</script>
+
+
+<!-- HTML -->
+<template>
+  <h1 v-if="exibir">Hello World!</h1>
+</template>
