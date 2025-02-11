ead45cd4b5e1b68c5ee2b9550fbc6be86a074bc9
Itamar Junior <cdajuniorf@gmail.com>
2025-02-10 21:55:44 -0300

    feat: Evento de clique.

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
 // import Exemplo03 from './componentes/Exemplo03Laco.vue';
 // import Exemplo04 from './componentes/Exemplo04HTML.vue';
 // import Exemplo05 from './componentes/Exemplo05Model.vue';
-import Exemplo06 from './componentes/Exemplo06Classe.vue';
-
+// import Exemplo06 from './componentes/Exemplo06Classe.vue';
+import Exemplo07 from './componentes/Exemplo07Clique.vue';
 </script>
 
 <!-- HTML -->
 <template>
-  <Exemplo06 />
+  <Exemplo07 />
 </template>
