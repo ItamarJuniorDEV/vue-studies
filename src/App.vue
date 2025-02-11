87b74a8856f6b74921f061dcf30a505249d65c6f
Itamar Junior <cdajuniorf@gmail.com>
2025-02-10 22:04:21 -0300

    feat: Eventos de teclado.

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,11 @@
 // import Exemplo04 from './componentes/Exemplo04HTML.vue';
 // import Exemplo05 from './componentes/Exemplo05Model.vue';
 // import Exemplo06 from './componentes/Exemplo06Classe.vue';
-import Exemplo07 from './componentes/Exemplo07Clique.vue';
+// import Exemplo07 from './componentes/Exemplo07Clique.vue';
+import Exemplo08 from './componentes/Exemplo08Teclado.vue';
 </script>
 
 <!-- HTML -->
 <template>
-  <Exemplo07 />
+  <Exemplo08 />
 </template>
