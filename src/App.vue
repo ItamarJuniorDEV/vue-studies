e3ce7c44cfeaefc07fbe11c47b6521243b7a6ac3
Itamar Junior <cdajuniorf@gmail.com>
2025-02-10 21:47:08 -0300

    feat: Interligações de classes.

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@
 // import Exemplo02 from './componentes/Exemplo02Condicional.vue';
 // import Exemplo03 from './componentes/Exemplo03Laco.vue';
 // import Exemplo04 from './componentes/Exemplo04HTML.vue';
-import Exemplo05 from './componentes/Exemplo05Model.vue';
+// import Exemplo05 from './componentes/Exemplo05Model.vue';
+import Exemplo06 from './componentes/Exemplo06Classe.vue';
+
 </script>
 
 <!-- HTML -->
 <template>
-  <Exemplo05 />
+  <Exemplo06 />
 </template>
---
+++ src/componentes/Exemplo06Classe.vue
@@ -0,0 +1,22 @@
+<!-- SCRIPT -->
+<script setup>
+// Variável
+let validacao = false;
+</script>
+
+<!-- CSS -->
+<style>
+.ok {
+  color: green;
+}
+
+.falha {
+  color: red;
+}
+</style>
+
+<!-- HTML -->
+<template>
+  <!-- Se a validação for verdadeira, será green; caso contrário, será red. -->
+  <p :class="validacao ? 'ok' : 'falha'">Utilizando a diretiva class</p>
+</template>
